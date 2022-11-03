f266202e9b4b91aa04d18bf34ddc0dcc0537f93c
Bruno <brunogiordano.ono@gmail.com>
2022-11-03 17:16:20 -0300

    feat(web): pages routing

---
+++ packages/web/src/pages/Login/Login.jsx
@@ -0,0 +1,34 @@
+import { Container, Login, Booking, BookingButton, Input, LoginButton, FormContainer, PasswordRecoverButton, RecoverContainer } from './styles';
+import LogoFacamp from '../../assets/logoFacamp.png'
+
+export const LoginPage = () => {
+  return (
+    <>
+      <Container>
+        <Booking>
+            <img src={LogoFacamp} alt="Logotipo Facamp" />
+          <h2>MAKERLAB</h2>
+          <BookingButton>RESERVAS</BookingButton>
+        </Booking>
+        <Login>
+          <h2>Login</h2>
+          <FormContainer>
+            <div>   
+                <label htmlFor='userContainer'>Usuário</label><br />
+                <Input placeholder='Digite seu usuário' type='text' id='userContainer'/>
+            </div>
+            <div>
+                <label htmlFor='passwordContainer'>Senha</label><br />
+                <Input placeholder='Digite sua senha' type='password' id='passwordContainer'/><br />
+                <RecoverContainer>
+                    <PasswordRecoverButton>Esqueceu a senha?</PasswordRecoverButton>
+                </RecoverContainer>
+            </div>
+                <LoginButton>Entrar</LoginButton>
+          </FormContainer>
+        </Login>
+      </Container>
+    </>
+  );
+};
+
--- packages/web/src/pages/Login/styles.js
+++ packages/web/src/pages/Login/styles.js
@@ -1,4 +1,4 @@
-import { styled } from './theme';
+import { styled } from '../../styles/theme';
 
 export const Container = styled('div', {
   display: 'flex',
