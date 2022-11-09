92faadcbafe8188f9ab0f1b49a5676cbe40f1e5c
Bruno <bruno0410@localhost.localdomain>
2022-11-09 14:07:39 -0300

    feat(web): development of Booking Page

--- packages/web/src/pages/UserScreen/UserScreen.jsx
+++ packages/web/src/pages/UserScreen/UserScreen.jsx
@@ -1,19 +1,32 @@
-import { ArrowBack, ArrowFoward, Logout, LogoutDiv, Month, UpperMenu } from './styles';
+import { ArrowBack, ArrowForward, DateControl, Logout, LogoutDiv, MainDiv, Month, TimeTable, UpperMenu } from './styles';
 import LogoFacamp from '../../assets/facampLogo.png'
+import ArrowIconF from '../../assets/arrowForward.svg'
+import ArrowIconB from '../../assets/arrowBack.svg'
+import LogoutIcon from '../../assets/logout.svg'
 
 export const BookingPage = () => {
   return (
     <>
       <UpperMenu>
         <img src={LogoFacamp} alt="Logotipo Facamp" />
-        <ArrowBack>Flecha</ArrowBack>
-        <ArrowFoward>Flecha</ArrowFoward>
-        <Month>Outubro</Month>
-        <h2>de 2022</h2>
+        <DateControl>
+          <ArrowBack>
+            <img src={ArrowIconB} alt="Logo Flecha pra trás" />
+          </ArrowBack>
+          <ArrowForward>
+            <img src={ArrowIconF} alt="Logo Flecha pra frente" />
+          </ArrowForward>
+          <Month>Outubro</Month>
+          <h2>de 2022</h2>
+        </DateControl>
+        
         <LogoutDiv>
-            <Logout>Logout</Logout>
+          <img src={LogoutIcon} alt="Logo Logout" />
         </LogoutDiv>
       </UpperMenu>
+      <MainDiv>
+        <h1>OI</h1>
+      </MainDiv>
     </>
   );
 };
--- packages/web/src/pages/UserScreen/styles.js
+++ packages/web/src/pages/UserScreen/styles.js
@@ -3,50 +3,58 @@ import { styled } from '../../styles/theme';
 export const UpperMenu = styled('div', {
     display: 'flex',
     width: '100%',
-    height: '6vh',
+    height: '10vh',
     backgroundColor: '$gray_200',
-  });
+});
+
+export const MainDiv = styled('div', {
+    height: '94%',
+    width: '100%',
+    backgroundColor: '$blue_800',
+});
 
 export const LogoFacamp = styled('img', {
     height: 'auto',
     maxWidth: '100%',
 });
 
-export const ArrowBack = styled('button', {
-    marginLeft: '5rem',
-    px: '1rem',
-    //backgroundImage: url("https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"),
-    //<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    '&:hover': {
-        backgroundColor: '$gray_300',
-        transition: '0.5s',
-        cursor: 'pointer',
-    },
+export const DateControl = styled('div', {
+    marginLeft: '2rem',
+    display: 'flex',
 })
 
-export const ArrowFoward = styled('button', {
-    marginLeft: '1rem',
-    px: '1rem',
-    //backgroundImage: url("https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"),
-    //<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+export const ArrowBack = styled('button', {
+    backgroundColor: '$gray_200',
+    borderRadius: '24px',
     '&:hover': {
         backgroundColor: '$gray_300',
         transition: '0.5s',
         cursor: 'pointer',
     },
-})
+});
+
+export const ArrowForward = styled('button', {
+    backgroundColor: '$gray_200',
+    borderRadius: '24px',
+    '&:hover': {
+        backgroundColor: '$gray_300',
+        transition: '0.5s',
+        cursor: 'pointer',
+    },
+});
 
 export const Month = styled('button', {
     color: '$blue_800',
     fontSize: '$md',
     fontWeight: '700',
+    backgroundColor: '$gray_200',
     '&:hover': {
         color: '$blue_900',
         backgroundColor: '$gray_300',
         transition: '0.5s',
         cursor: 'pointer',
     },
-})
+});
 
 export const LogoutDiv = styled('div', {
     width: '100%',
@@ -62,4 +70,12 @@ export const Logout = styled('button', {
         transition: '0.5s',
         cursor: 'pointer',
     },
-})
+});
+
+export const TimeTable = styled('div', {
+    backgroundColor: '$gray_100',
+    height: '95%',
+    width: '97%',
+    mx: 'auto',
+    my: 'auto', //Não está alinhando verticalmente
+});
